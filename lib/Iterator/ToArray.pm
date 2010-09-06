1f0680f418a9f5444f8e89b2e228ecfd5be91651
Yoshihiro Sasaki <aloelight@gmail.com>
2010-09-06 21:02:49 +0900

    initial commit

---
+++ lib/Iterator/ToArray.pm
@@ -0,0 +1,32 @@
+package Iterator::ToArray;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Iterator::ToArray -
+
+=head1 SYNOPSIS
+
+  use Iterator::ToArray;
+
+=head1 DESCRIPTION
+
+Iterator::ToArray is
+
+=head1 AUTHOR
+
+aloelight E<lt>aloelight {at} gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
